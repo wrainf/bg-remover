712ce524b3c79f2ea228b64f22c849e8a9716c68
wrainf <amirfaiz1806@gmail.com>
2023-07-01 23:45:59 +0100

    update frontend for new api structure

--- client/script.js
+++ client/script.js
@@ -1,20 +1,38 @@
 const form = document.getElementById("imageForm")
 const resultContainer = document.getElementById('result')
 const loader = document.getElementById('loading')
+let imageID;
 let imageURL;
+let interval;
 form.addEventListener('submit', async function(event) {
   event.preventDefault()
-  loading.className = 'loading-container'
+  loader.className = 'loading-container'
   const formData = new FormData(form)
-  console.log(formData)
-  const res = await fetch('https://bg-remover-h8wh.onrender.com/upload', {method: 'POST', body: formData})
-  const blob = await res.blob()
-  imageURL = await URL.createObjectURL(blob)
-  const imageElement = document.createElement('img');
-  imageElement.src = imageURL;
-  loading.className = 'hidden'
-  resultContainer.appendChild(imageElement);
+  const res = await fetch('http://localhost:5000/upload', {method: 'POST', body: formData})
+  const json = await res.json()
+  imageID = json.id
+  interval = setInterval(getImage, 5000);
 })
+
+async function getImage(){
+  const res = await fetch(`http://localhost:5000/fetch/${imageID}`);
+  if(res.ok){
+    const blob = await res.blob()
+    if(blob.size > 0) {
+      imageURL = await URL.createObjectURL(blob)
+      console.log(imageURL);
+      const imageElement = document.createElement('img');
+      imageElement.src = imageURL;
+      resultContainer.prepend(imageElement);
+      clearInterval(interval)
+      loader.className = 'hidden'
+    }
+    
+  } else {
+    alert("Image processing failed")
+  }
+  
+}
 
 resultContainer.addEventListener('click', function() {
   const link = document.createElement('a')
